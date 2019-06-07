d8ce0776762a041702a7290e33857253be90d406
qianyi.zj <qianyi.zj@alibaba-inc.com>
2019-06-07 23:25:50 +0800

    class转化function部分实现

--- test/traverse/fixtures/class/constructor/expected.js
+++ test/traverse/fixtures/class/constructor/expected.js
@@ -1,5 +1,8 @@
 var A = function () {
     function A() {
+        this.a = 1;
+        this.xxx = function () {
+        };
     }
     return A;
 }();
--- testAst.js
+++ testAst.js
@@ -1,2 +1,10 @@
 const acorn = require('./lib/acorn');
-console.dir(acorn.parse('{let a=1}'), {depth: 20});
+// console.dir(acorn.parse('{let a=1}'), {depth: 20});
+
+
+var {Map} = require('immutable')
+var b = Map({a: 1})
+
+var c = b.toJS()
+
+console.log(c)
