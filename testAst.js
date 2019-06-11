566fc9e6d16807e63aaecbf039a8452e22d7667c
qianyi.zj <qianyi.zj@alibaba-inc.com>
2019-06-11 19:33:29 +0800

    ClassDeclaration static解析

--- testAst.js
+++ testAst.js
@@ -1,4 +1,4 @@
 const acorn = require('./lib/acorn');
 // console.dir(acorn.parse('{let a=1}'), {depth: 20});
 //class A{set a(x){this.x=x;}}
-console.dir(acorn.parse('class Test {get test() {return 5 + 5;}}'),{depth:20})
+console.dir(acorn.parse('class A extends B{x(){};;static y(){}}'),{depth:20})
