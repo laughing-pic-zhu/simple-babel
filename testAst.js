7aa604b13a68338640ba8b34a49688dcc5648fd6
qianyi.zj <qianyi.zj@alibaba-inc.com>
2019-06-30 10:11:18 +0800

    支持for of转化ast

--- testAst.js
+++ testAst.js
@@ -1,3 +1,3 @@
 const acorn = require('./lib/acorn');
 // console.dir(acorn.parse('export { encrypt as enc } from "crypto";'),{depth:20})
-console.dir(acorn.parse('var a=`b${c}d${e}`'), {depth: 20})
+console.dir(acorn.parse('for (var a of b){}'), {depth: 20})
